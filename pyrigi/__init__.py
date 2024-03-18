5b2ec79195117a2a79c5fd7de38c81c9a03d39e9
Jan Legersky <jan.legersky@gmail.com>
2024-03-18 14:48:23 +0100

    content of PyRigi moved to __init__

--- pyrigi/__init__.py
+++ pyrigi/__init__.py
@@ -1,7 +1,17 @@
 """
-This is the initialization module. 
+This is a package for the rigidity and flexibility of graphs and bar-and-joint frameworks. 
+
+AUTHORS: 
+Matteo Gallet
+Georg Grasegger
+Matthias Himmelmann
+Jan Legerský
+
 """
+
 
 import numpy as np 
 import sympy as sp
 from networkx import Graph
+
+from pyrigi.framework import Framework
