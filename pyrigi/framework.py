3bef716e49377660c4561105aea42c473425425b
Matteo  Gallet <matteo.gallet@units.it>
2024-03-19 12:33:40 +0100

    Fixed bug in Framework.

--- pyrigi/framework.py
+++ pyrigi/framework.py
@@ -44,7 +44,7 @@ class Framework(object):
     def __init__(self, graph, realization):
         # TODO: check that graph and realization is not empty
         assert isinstance(graph, Graph)
-        dimension = len(realization.values()[0])
+        dimension = len(list(realization.values())[0]
         for v in graph.vertices():
             assert v in realization
             assert len(realization[v])==dimension
