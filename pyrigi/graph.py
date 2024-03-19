6402a19caf077770f7121b2c77c09fe0a7139031
Jan Legersky <jan.legersky@gmail.com>
2024-03-19 09:49:39 +0100

    graph.py

---
+++ pyrigi/graph.py
@@ -0,0 +1,18 @@
+""""
+Module for rigidity related graph properties.
+"""
+
+
+import networkx as nx
+
+class Graph(nx.Graph):
+    '''
+    Class representing a graph.
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
