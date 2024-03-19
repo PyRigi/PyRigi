495f54e0dd6a1b7e8c7fece836c0b30cc89286f7
Matteo  Gallet <matteo.gallet@units.it>
2024-03-19 10:43:32 +0100

    Used new Graph class into Framework. Inserted the class Realization into Framework.

--- pyrigi/framework.py
+++ pyrigi/framework.py
@@ -10,77 +10,79 @@ Classes:
 
     Framework
     Realization
-    
-    
+
+
 
 
 """
 
+from pyrigi.graph import Graph
 
-import networkx as nx
 
 
-class Realization(object):
-    r"""
-    This class represents a realization.
-    
-    A realization is a map from the set of vertices to $\RR^d$. 
-    The labeling is implicit and given by the placement's order.
-
-    METHODS:
-    
-    .. autosummary::
-        add_vertex
-        add_vertex_list
-        
-    
-    """
-    placement = None
-    dimension = None
-
-    def __init__(self, points=[], dim=2):
-        for vector in points:
-            assert(len(vector))==dim
-        self.placement = points
-        self.dimension = dim
-    
-    def add_vertex(self, point):
-        assert len(point)==self.dimension
-        self.placement.append(point)
-
-    def add_vertex_list(self, points):
-        for vector in points:
-            assert(len(vector))==self.dimension
-        self.placement.append(points)
-
-    
 class Framework(object):
     """
     This class provides the functionality for frameworks. By definition, it is a tuple of a graph and a realization.
-    
+
     METHODS:
-    
+
     .. autosummary::
+        dim
+        dimension
         add_vertex
-        add_vertex_list
+        add_vertices
         add_edge
-        add_edge_list
+        add_edges
     """
     graph = None
     realization = None
 
+    class Realization(dict):
+        r"""
+        This class represents a realization.
+
+        A realization is a map from the set of vertices to $\RR^d$.
+        The labeling is implicit and given by the placement's order.
+
+        METHODS:
+
+        .. autosummary::
+            add_vertex
+            add_vertices
+        """
+
+        def __init__(self, vertices=[], points=[], dim=2):
+            self.add_vertices(vertices, points)
+            self.dimension = dim
+
+        def add_vertex(self, vertex, point):
+            assert len(point)==self.dimension
+            self[vertex] = point
+
+        def add_vertices(self, vertices, points):
+            assert len(vertices)==len(points)
+            for vector in points:
+                assert(len(vector))==self.dimension
+            self.update(zip(vertices, points))
+
     def __init__(self, p=[], d=2):
         self.realization = Realization(p, d)
-        self.graph = nx.Graph()
-    
+        self.graph = Graph()
+
+    def dim(self):
+        return self.dimension()
+
+    def dimension(self):
+        return self.realization.dimension
+
     def add_vertex(self, point, label=None):
         if label == None:
             maxNode = max(self.graph.nodes) if len(self.graph.nodes)>0 else 0
             label = maxNode + 1
         self.realization.add_vertex(point)
         self.graph.add_node(label)
-        
-    def add_vertex_list(self, points, labels=[]):
+
+    def add_vertices(self, points, labels=[]):
         assert(len(points)==len(labels) or len(labels)==0)
         if len(labels)==0:
             for point in points:
@@ -94,6 +96,6 @@ class Framework(object):
         assert (edge[0] in self.graph.nodes and edge[1] in self.graph.nodes)
         self.graph.add_edge(*(edge))
 
-    def add_edge_list(self, edges):
+    def add_edges(self, edges):
         for edge in edges:
             self.add_edge(edge)
