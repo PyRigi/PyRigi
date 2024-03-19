0c302b5ac73aa55705ac923a1d8532ecfec832e0
Matteo  Gallet <matteo.gallet@units.it>
2024-03-19 12:54:23 +0100

    Graph of a framework as private.

--- pyrigi/graph.py
+++ pyrigi/graph.py
@@ -16,16 +16,11 @@ class Graph(nx.Graph):
         Constructor for Graph.
         It accepts a networkx graph as data, or a list of edges, or a pair of vertices and edges.
         '''
-        self._part_of_framework = False
 
     def delete_vertex(self, vertex):
-        if self._part_of_framework:
-            raise AttributeError("This graph is part of a framework. Please use the corresponding method of the framework.")
         raise NotImplementedError()
 
     def delete_vertices(self, vertices):
-        if self._part_of_framework:
-            raise AttributeError("This graph is part of a framework. Please use the corresponding method of the framework.")
         raise NotImplementedError()
 
     def delete_edge(self, edge):
