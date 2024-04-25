43bb309888bfe83218e5c9ee93bbc30d6d35c225
Matthias Himmelmann <65544132+matthiashimmelmann@users.noreply.github.com>
2024-04-25 16:48:40 +0200

    Added several new methods to `Framework` + ´is_infinitesimally_rigid()` and ´is_minimally_rigid()` including a test + `infinitesimal_flexes()`, `nontrivial_infinitesimal_flexes()` and `trivial_infinitesimal_flexes()` including a test + added a few convenience methods such as `delete_edge()`, `delete_vertex()`

--- pyrigi/framework.py
+++ pyrigi/framework.py
@@ -14,8 +14,10 @@ Classes:
 from __future__ import annotations
 
 from copy import deepcopy
+from itertools import combinations
 import networkx as nx
 from pyrigi.graph import Graph
+from random import randrange
 from sympy import Matrix, flatten
 from pyrigi.data_type import Vertex, Edge, Point, List, Any, Dict
 
@@ -123,19 +125,18 @@ class Framework(object):
         raise NotImplementedError()
 
     def delete_vertex(self, vertex: Vertex) -> None:
-        raise NotImplementedError()
+        self._graph.delete_vertex(vertex)
+        del self._realization[vertex]
 
     def delete_vertices(self, vertices: List[Vertex]) -> None:
-        raise NotImplementedError()
-
+        for vertex in vertices:
+            self.delete_vertex(vertex)
+    
     def delete_edge(self, edge: Edge) -> None:
-        raise NotImplementedError()
-
+        self._graph.delete_edge(edge)
+    
     def delete_edges(self, edges: List[Edge]) -> None:
-        raise NotImplementedError()
-
-    def set_vertex_position(self, vertex: Vertex, point: Point) -> None:
-        raise NotImplementedError()
+        self._graph.delete_edges(edges)        
 
     def get_realization_list(self) -> List[Point]:
         """
@@ -161,13 +162,19 @@ class Framework(object):
         assert vertex in self._realization
         assert len(point) == self._dim
         self._realization[vertex] = point
+    
+    def set_vertex_position(self, vertex: Vertex, point: Point) -> None:
+        self.change_vertex_coordinates(vertex, point)
 
-    def change_vertex_coordinates_list(self, vertices: List[Vertex], points: List[Point]):
+    def change_vertex_coordinates_list(self, vertices: List[Vertex], points: List[Point]) -> None:
         if list(set(vertices)).sort() != list(vertices).sort():
             raise AttributeError("Mulitple Vertices with the same name were found!")
         assert len(vertices) == len(points)
         for i in range(0, len(vertices)):
             self.change_vertex_coordinates(vertices[i], points[i])
+
+    def set_vertex_positions(self, vertices: List[Vertex], points: List[Point]) -> None:
+        self.change_vertex_coordinates_list(vertices, points)
 
     def change_realization(self, subset_of_realization: Dict[Vertex, Point]):
         self.change_vertex_coordinates_list(subset_of_realization.keys(), subset_of_realization.values())
@@ -210,11 +217,27 @@ class Framework(object):
         r""" Construct the stress matrix from a stress of from its support
         """
         raise NotImplementedError()
+    
+    def trivial_infinitesimal_flexes(self) -> List[Matrix]:
+        r"""The complete graph is infinitesimally rigid in all dimensions. Thus, for computing the trivial
+        flexes it suffices to compute all infinitesimal flexes of the complete graph."""
+        K = nx.complete_graph(len(self._graph.vertices()))
+        F = Framework(graph = Graph(K.edges), realization = self.realization(), dim=self.dim())
+        return F.infinitesimal_flexes(include_trivial = True)
 
-    def infinitesimal_flexes(self, trivial: bool = False) -> Any:
-        r""" Returns a basis of the space of infinitesimal flexes
+    def nontrivial_infinitesimal_flexes(self) -> List[Matrix]:
+        return self.infinitesimal_flexes(include_trivial = False)
+
+    def infinitesimal_flexes(self, include_trivial: bool = False) -> List[Matrix]:
+        r""" Returns a basis of the space of infinitesimal flexes. This is done by orthogonalizing the 
+        space of trivial and non-trivial flexes and subsequently forgetting the trivial flexes.
         """
-        raise NotImplementedError()
+        if include_trivial:
+            return self.rigidity_matrix().nullspace()
+        trivial_flexes = self.trivial_infinitesimal_flexes()
+        all_flexes = self.rigidity_matrix().nullspace()
+        basis_flexspace = Matrix.orthogonalize(*(trivial_flexes + all_flexes), rankcheck = False)
+        return basis_flexspace[len(trivial_flexes):len(all_flexes)+1]
 
     def stresses(self) -> Any:
         r""" Returns a basis of the space of stresses
@@ -225,16 +248,25 @@ class Framework(object):
         return self.rigidity_matrix().rank()
 
     def is_infinitesimally_rigid(self) -> bool:
-        raise NotImplementedError()
+        return len(self.graph().vertices()) <= 1 or self.rigidity_matrix_rank() == self.dim() * len(self.graph().vertices()) - (self.dim()) * (self.dim()+1) // 2
 
     def is_infinitesimally_spanning(self) -> bool:
         raise NotImplementedError()
 
     def is_minimally_infinitesimally_rigid(self) -> bool:
-        raise NotImplementedError()
+        """A framework is called minimally infinitesimally rigid, if it is infinitessimally rigid 
+        and the removal of any edge results in an infinitesimally flexible graph."""
+        if not self.is_infinitesimally_rigid():
+            return False
+        for edge in self.graph().edges:
+            F = deepcopy(self)
+            F.delete_edge(edge)
+            if F.is_infinitesimally_rigid():
+                return False
+        return True
 
     def is_infinitesimally_flexible(self) -> bool:
-        raise NotImplementedError()
+        return not self.is_infinitesimally_rigid()
 
     def is_independent(self) -> bool:
         raise NotImplementedError()
@@ -259,7 +291,4 @@ class Framework(object):
         raise NotImplementedError()
 
     def pin(self, vertices: List[Vertex]) -> None:
-        raise NotImplementedError()
-
-    def trivial_infinitesimal_flexes(self) -> Any:
         raise NotImplementedError()
--- test/test_framework.py
+++ test/test_framework.py
@@ -2,7 +2,7 @@ from pyrigi.graph import Graph
 from pyrigi.framework import Framework
 
 def test_dimension():
-    F = Framework(Graph([[0,1]]),{0:[1,2], 1:[0,5]})
+    F = Framework(Graph([[0,1]]), {0:[1,2], 1:[0,5]})
     assert F.dim() == F.dimension()
     assert F.dim() == 2
     F = Framework(dim=3)
@@ -34,3 +34,17 @@ def test_vertex_addition():
     )
     F_.add_edge([0, 1])
     plt = F_.draw_framework()
+
+def test_inf_rigidity():
+    F = Framework(Graph([[0,1], [1,2], [0,2]]), {0:[1,2], 1:[0,5], 2:[3,3]})
+    assert (F.is_infinitesimally_rigid() and F.is_minimally_infinitesimally_rigid())
+    F_ = Framework(Graph([[0,1], [1,2], [2,3], [3,0], [0,2], [1,3]]), {0:[0,0], 1:[2,0], 2:[2,2], 3:[-1,1]})
+    assert (F_.is_infinitesimally_rigid() and not F_.is_minimally_infinitesimally_rigid())
+    F_.delete_edge([0,2])
+    assert (F_.is_infinitesimally_rigid() and F_.is_minimally_infinitesimally_rigid())
+
+def test_inf_motions():
+    F = Framework(Graph([[0,1]]), {0:[0,0], 1:[1,0]})
+    assert F.infinitesimal_flexes(include_trivial = True) == F.trivial_infinitesimal_flexes()
+    F_ = Framework(Graph([[0,1], [1,2], [2,3], [0,3]]), {0:[0,0], 1:[1,0], 2:[1,1], 3:[0,1]})
+    assert (len(F_.infinitesimal_flexes())==1 and F_.infinitesimal_flexes()[0].rank()==1)
