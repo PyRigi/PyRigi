72bbe8f186084efae40e8eb8893b43cd4c165ed2
Matteo Gallet <matteo.gallet@units.it>
2024-05-09 22:23:49 +0200

    Ignored conf.py for flake8.

--- doc/conf.py
+++ doc/conf.py
@@ -6,6 +6,8 @@
 # full list see the documentation:
 # http://www.sphinx-doc.org/en/master/config
 
+# flake8: noqa
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
