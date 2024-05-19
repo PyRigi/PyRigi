0811fd4140bdd39a7bfa556ec7231aaeaba1d4e8
Matthias Himmelmann <65544132+matthiashimmelmann@users.noreply.github.com>
2024-05-19 22:15:36 +0200

    Lint update

--- pyrigi/framework.py
+++ pyrigi/framework.py
@@ -117,7 +117,8 @@ class Framework(object):
 
     def __getitem__(self, vertex) -> Point:
         """
-        Return the coordinates corresponding to the image of a given vertex under the realization map.
+        Return the coordinates corresponding to the image 
+        of a given vertex under the realization map.
 
         Examples
         --------
