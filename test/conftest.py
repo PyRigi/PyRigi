f46aeadb0eabf601d5085bf7383593bc30516512
Jan Legersky <jan.legersky@gmail.com>
2024-05-22 11:41:55 +0200

    some 1D frameworks

--- test/conftest.py
+++ test/conftest.py
@@ -59,10 +59,24 @@ def P3():
 # --------------------------------------------------------------
 
 
+# Empty
+@pytest.fixture
+def Empty_d1(Empty):
+    """1-dimensional empty framework"""
+    return Framework.Empty(1)
+
+
 @pytest.fixture
 def Empty_d2(Empty):
     """2-dimensional empty framework"""
     return Framework.Empty(2)
+
+
+# C4
+@pytest.fixture
+def C4_d1(C4):
+    """Framework of the 4-cycle with square realization in the plane"""
+    return Framework(C4, {0: [0], 1: [1], 2: [2], 3: [3]})
 
 
 @pytest.fixture
@@ -71,10 +85,18 @@ def C4_d2_square(C4):
     return Framework(C4, {0: [0, 0], 1: [1, 0], 2: [1, 1], 3: [0, 1]})
 
 
+# Diamond
 @pytest.fixture
 def Diamond_d2_square(Diamond):
     """Framework of the diamond with square realization in the plane"""
     return Framework(Diamond, {0: [0, 0], 1: [1, 0], 2: [1, 1], 3: [0, 1]})
+
+
+# K2
+@pytest.fixture
+def K2_d1(K2):
+    """Framework of K2 on the line"""
+    return Framework(K2, {0: [0, 0], 1: [1, 0]})
 
 
 @pytest.fixture
@@ -83,22 +105,44 @@ def K2_d2(K2):
     return Framework(K2, {0: [0, 0], 1: [1, 0]})
 
 
+# K3
+@pytest.fixture
+def K3_d1(K3):
+    """Framework of K3 on the line"""
+    return Framework(K3, {0: [0], 1: [1], 2: [2]})
+
+
 @pytest.fixture
 def K3_d2_rightangle(K3):
     """Framework of K3 with right-angle triangle realization in the plane"""
     return Framework(K3, {0: [0, 0], 1: [1, 0], 2: [0, 1]})
 
 
+# K4
 @pytest.fixture
 def K4_d2(K4):
     """Framework of K4 with square realization in the plane"""
     return Framework(K4, {0: [0, 0], 1: [1, 0], 2: [1, 1], 3: [0, 1]})
 
 
+# P2
+@pytest.fixture
+def P2_d1(P2):
+    """Framework of P2 with 2 edges on the x-axis in the plane"""
+    return Framework([[i, i + 1] for i in range(2)], {i: [i] for i in range(2)})
+
+
 @pytest.fixture
 def P2_d2_linear(P2):
     """Framework of P2 with 2 edges on the x-axis in the plane"""
     return Framework([[i, i + 1] for i in range(2)], {i: [i, 0] for i in range(2)})
+
+
+# P3
+@pytest.fixture
+def P3_d1(P3):
+    """Framework of P3 with 3 edges on the x-axis in the plane"""
+    return Framework([[i, i + 1] for i in range(2)], {i: [i] for i in range(2)})
 
 
 @pytest.fixture
