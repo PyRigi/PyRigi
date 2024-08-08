78acfdd51d3940e4b62134e6d7e0d72e3e2265c6
Jan Legersky <jan.legersky@gmail.com>
2024-08-08 18:11:31 +0200

    0.1.0 -> 0.2.0

--- doc/conf.py
+++ doc/conf.py
@@ -26,9 +26,9 @@ copyright = "2024, The PyRigi Developers"
 author = "The PyRigi Developers"
 
 # The short X.Y version
-version = "0.1"
+version = "0.2"
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.2.0"
 
 
 # -- General configuration ---------------------------------------------------
