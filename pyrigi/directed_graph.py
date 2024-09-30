3c711d7675fda57bde7c99d3fafff03c39588c81
Andras Mihalyko <mihalykoandras@gmail.com>
2024-09-30 19:22:36 +0200

    Change DFS to pass edges to be reversed later

--- pyrigi/directed_graph.py
+++ pyrigi/directed_graph.py
@@ -24,21 +24,18 @@ class MultiDiGraph(nx.MultiDiGraph):
     """
     Class representing a directed graph.
     All nx methods need a wrapper - to make future developments easier. 
-    Extra features needed on top of nx.MultiDiGraph:
-    - indegree of nodes
     """
 
     def in_degree(self, node):
         return super().in_degree(node)
     
-    def reverse_edge(self, edge, node_to):
+    def out_degree(self, node):
+        return super().out_degree(node)
+    
+    def point_edge_head_to(self, edge, node_to):
         # edge.get_edge_data()
         # self.remove_edge()
         # add reversed edge
-        return 0
-    
-    # runs a DFS from v1 and v2 with K and L 
-    def run_DFS(self, v1, v2, K, L):
         return 0
 
     # get the list of nodes accessible from v1 and v2 
@@ -49,40 +46,49 @@ class MultiDiGraph(nx.MultiDiGraph):
     # can you add the edge between the nodes v1 and v2, 
     # so that it still respects the node degrees?   
     def can_add_edge_between_nodes(self, u, v, K, L):
-        def dfs(node, visited, path):
+        def dfs(node, visited, path, edge_path, current_edge = None):
             visited.add(node)
             path.append(node)
+            if current_edge:
+                edge_path.append(current_edge)
 
             # Check if the stopping criteria is met
             if node != u and node != v and G.out_degree(node) < K:
+                # turn around edges via path
                 for i in range(len(path) - 1):
                     self.remove_edge(path[i], path[i + 1])
                     self.add_edge(path[i + 1], path[i])
+                #for i in range(len(path) - 1):
+                #    self.point_edge_head_to(edge_path[i], path[i])
                 return True
 
-            for neighbor in self.neighbors(node):
+            for out_edge in self.out_edges(node):
                 found = False
-                if neighbor not in visited:
-                    found = dfs(neighbor, visited, path)
+                next_node = out_edge[-1]
+                if next_node not in visited:
+                    found = dfs(next_node, visited, path, edge_path, out_edge)
                 if found:
                     return True
 
             path.pop()
+            if edge_path:
+                edge_path.pop()
             return False
 
         max_degree_for_u_and_v_together = 2 * K - L - 1
         while self.out_degree(u) + self.out_degree(v) > max_degree_for_u_and_v_together:
-            print(self.edges())
+            #print(self.edges())
             visited_u, visited_v = set(), set()
             path_u, path_v = [], []
+            edge_path_u, edge_path_v = [], []
 
             # Perform DFS from u
-            found_from_u = dfs(u, visited_u, path_u)
+            found_from_u = dfs(u, visited_u, path_u, edge_path_u)
             if found_from_u:
                 continue
 
             # Perform DFS from v
-            found_from_v = dfs(v, visited_v, path_v)
+            found_from_v = dfs(v, visited_v, path_v, edge_path_v)
             if found_from_v:
                 continue
             # not found_from_u and not found_from_v
@@ -101,9 +107,17 @@ u, v, K, L = 2, 4, 2, 3
 result = G.can_add_edge_between_nodes(u, v, K, L)
 print(result)
 
+result = G.can_add_edge_between_nodes(1, 3, K, L)
+print(result)
+
+
 G.add_edges_from([(2, 4)])
+
 result = G.can_add_edge_between_nodes(u, v, K, L)
 print(result)
+result = G.can_add_edge_between_nodes(1, 3, K, L)
+print(result)
+
 
 
 
