8d9b918982a8cfa86a35a4de9886eaef0c8a3927
Andras Mihalyko <mihalykoandras@gmail.com>
2024-09-30 19:22:36 +0200

    use edge path instead of path

--- pyrigi/directed_graph.py
+++ pyrigi/directed_graph.py
@@ -21,10 +21,10 @@ class MultiDiGraph(nx.MultiDiGraph):
         return super().out_degree(node)
     
     def point_edge_head_to(self, edge, node_to):
-        # edge.get_edge_data()
-        # self.remove_edge()
-        # add reversed edge
-        return 0
+        tail = edge[0]
+        head = edge[1]
+        self.remove_edge(tail, head)
+        self.add_edge(head, node_to)
 
     # get the list of nodes accessible from v1 and v2 
     # that respects the indegree rules 
@@ -34,30 +34,26 @@ class MultiDiGraph(nx.MultiDiGraph):
     # can you add the edge between the nodes v1 and v2, 
     # so that it still respects the node degrees?   
     def can_add_edge_between_nodes(self, u, v, K, L):
-        def dfs(node, visited, path, edge_path, current_edge = None):
+        def dfs(node, visited, edge_path, current_edge = None):
             visited.add(node)
-            path.append(node)
             if current_edge:
                 edge_path.append(current_edge)
 
             # Check if the stopping criteria is met
             if node != u and node != v and self.out_degree(node) < K:
                 # turn around edges via path
-                for i in range(len(path) - 1):
-                    self.remove_edge(path[i], path[i + 1])
-                    self.add_edge(path[i + 1], path[i])
-                #for i in range(len(path) - 1):
-                #    self.point_edge_head_to(edge_path[i], path[i])
+                for edge in edge_path:
+                    self.point_edge_head_to(edge, edge[0])
+
                 return True
 
             for out_edge in self.out_edges(node):
                 found = False
                 next_node = out_edge[-1]
                 if next_node not in visited:
-                    found = dfs(next_node, visited, path, edge_path, out_edge)
+                    found = dfs(next_node, visited, edge_path, out_edge)
                 if found:
                     return True
-            path.pop()
             if edge_path:
                 edge_path.pop()
             return False
@@ -66,16 +62,15 @@ class MultiDiGraph(nx.MultiDiGraph):
         while self.out_degree(u) + self.out_degree(v) > max_degree_for_u_and_v_together:
             #print(self.edges())
             visited = {u,v}
-            path_u, path_v = [], []
             edge_path_u, edge_path_v = [], []
 
             # Perform DFS from u
-            found_from_u = dfs(u, visited, path_u, edge_path_u)
+            found_from_u = dfs(u, visited, edge_path_u)
             if found_from_u:
                 continue
 
             # Perform DFS from v
-            found_from_v = dfs(v, visited, path_v, edge_path_v)
+            found_from_v = dfs(v, visited, edge_path_v)
             if found_from_v:
                 continue
             # not found_from_u and not found_from_v
