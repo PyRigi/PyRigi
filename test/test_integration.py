0cf7fdb49e0d51c1c035926b90fbbd546dc188d1
Andras Mihalyko <mihalykoandras@gmail.com>
2024-11-05 22:34:04 +0100

    add large tests for sparse

--- test/test_integration.py
+++ test/test_integration.py
@@ -35,18 +35,35 @@ def test_rigid_in_d2():
 @pytest.mark.large
 def test_big_random_tight_graphs():
     # (6,8)-tight graph on 50 vertices and 292 edges
-    graph = read_graph_from_file("test/input_graphs/random_6_8.txt")
+    graph = read_graph_from_file("test/input_graphs/tight_6_8.txt")
     assert graph.is_tight(K=6, L=8, algorithm="pebble")
 
     # (7,3)-tight graph on 70 vertices and 487 edges
-    graph = read_graph_from_file("test/input_graphs/random_7_3.txt")
+    graph = read_graph_from_file("test/input_graphs/tight_7_3.txt")
     assert graph.is_tight(K=7, L=3, algorithm="pebble")
 
     # (5,9)-tight graph on 40 vertices and 191 edges
-    graph = read_graph_from_file("test/input_graphs/random_5_9.txt")
+    graph = read_graph_from_file("test/input_graphs/tight_5_9.txt")
     assert graph.is_tight(K=5, L=9, algorithm="pebble")
 
     # (13,14)-tight graph on 20 vertices and 246 edges
-    graph = read_graph_from_file("test/input_graphs/random_13_14.txt")
+    graph = read_graph_from_file("test/input_graphs/tight_13_14.txt")
     assert graph.is_tight(K=13, L=14, algorithm="pebble")
 
+
+@pytest.mark.large
+def test_big_random_sparse_graphs():
+    # (3,1)-sparse graph on 20 vertices and 58 edges
+    graph = read_graph_from_file("test/input_graphs/sparse_3_1.txt")
+    assert graph.is_sparse(K=3, L=1, algorithm="pebble")
+
+    # (1,1)-sparse graph on 40 vertices and 38 edges - one edge broken
+    graph = read_graph_from_file("test/input_graphs/sparse_1_1.txt")
+    assert graph.is_sparse(K=1, L=1, algorithm="pebble")
+
+    # (17,30)-sparse graph on 50 vertices and 38 edges
+    graph = read_graph_from_file("test/input_graphs/sparse_17_30.txt")
+    assert graph.is_sparse(K=17, L=30, algorithm="pebble")
+
+    graph = read_graph_from_file("test/input_graphs/sparse_4_6.txt")
+    assert graph.is_sparse(K=4, L=6, algorithm="pebble")
