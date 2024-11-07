a53474f4fb324d14355bf6f3ab415d4962771438
Jan Rubes <rubesja5@fit.cvut.cz>
2024-11-07 11:17:03 +0100

    initial motion file

---
+++ pyrigi/motion.py
@@ -0,0 +1,13 @@
+"""
+This file contains functionality related to finite flexes.
+"""
+
+
+class Motion(object):
+    """Class representing a finite flex of a framework.
+
+    TODO: implement this class
+    """
+
+    def __init__(self):
+        pass
