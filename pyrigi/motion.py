691c15f30de4fda1af70b6bd7f6f81dbc174a9bf
Jan Rubes <rubesja5@fit.cvut.cz>
2024-11-09 12:16:23 +0100

    checking nodes in the given motion

--- pyrigi/motion.py
+++ pyrigi/motion.py
@@ -3,17 +3,9 @@ This file contains functionality related to finite flexes.
 """
 
 from pyrigi.graph import Graph
-from pyrigi.data_type import Vertex, Edge, Point
-from sympy import sympify, Symbol, Matrix, vector, simplify
+from pyrigi.data_type import Vertex, Point
+from sympy import simplify
 from pyrigi.misc import point_to_vector
-
-
-class Motion:
-    pass
-
-
-class ParametricMotion(Motion):
-    pass
 
 
 class Motion(object):
@@ -22,18 +14,24 @@ class Motion(object):
     """
 
     def __init__(self, graph: Graph) -> None:
+        """
+        Create an instance of a graph's motion.
+        """
+
         self._graph = graph
 
     def __str__(self) -> str:
-        return (
-            f"{self.__class__.__name__} of a " + self._graph.__str__()
-        )
+        return f"{self.__class__.__name__} of a " + self._graph.__str__()
 
     def __repr__(self) -> str:
         return self.__str__()
 
 
 class ParametricMotion(Motion):
+    """
+    Class representing a parametric motion.
+    """
+
     def __init__(
         self, graph: Graph, motion: dict[Vertex, Point], interval: tuple
     ) -> None:
@@ -42,17 +40,29 @@ class ParametricMotion(Motion):
         """
 
         super().__init__(graph)
+
+        if not len(motion) == self._graph.number_of_nodes():
+            raise IndexError(
+                "The realization does not contain the correct amount of vertices!"
+            )
+
+        for v in self._graph.nodes:
+            if v not in motion:
+                raise KeyError("Vertex {vertex} is not a key of the given realization!")
+
         self._parametrization = {i: point_to_vector(v) for i, v in motion.items()}
+
         symbols = set()
         for vertex, position in self._parametrization.items():
             for coord in position:
-                for sym in coord.free_symbols:
-                    if sym.is_Symbol:
-                        symbols.add(sym)
+                for symbol in coord.free_symbols:
+                    if symbol.is_Symbol:
+                        symbols.add(symbol)
 
         if len(symbols) != 1:
             raise ValueError(
-                f"Expected exactly one parameter in the motion! got: {len(symbols)} parameters."
+                "Expected exactly one parameter in the motion! got: "
+                f"{len(symbols)} parameters."
             )
 
         self._interval = interval
@@ -63,36 +73,51 @@ class ParametricMotion(Motion):
     def check_edge_lengths(self) -> bool:
         """
         Check whether the saved motion preserves edge lengths.
+
+        TODO
+        ----
+        Tests
+
         """
 
         for u, v in self._graph.edges:
-            l = self._parametrization[u] - self._parametrization[v]
-            l = l.T * l
-            l.simplify()
-            if l.has(self._parameter):
+            edge = self._parametrization[u] - self._parametrization[v]
+            edge_len = edge.T * edge
+            edge_len.simplify()
+            if edge_len.has(self._parameter):
                 return False
         return True
 
-    def get_realization(self, value, numeric: bool = True) -> dict[Vertex:Point]:
+    def get_realization(self, value, numeric: bool = False) -> dict[Vertex:Point]:
         """
-        Return specific realization for the given value of t.
+        Return specific realization for the given value of the parameter.
+
+        TODO
+        ----
+        Tests
+
         """
 
-        res = {}
+        realization = {}
         for v in self._graph.nodes:
+            placement = simplify(
+                self._parametrization[v].subs({self._parameter: value})
+            )
             if numeric:
-                res[v] = self._parametrization[v].subs({self._parameter: value}.evalf())
-            else:
-                res[v] = simplify(
-                    self._parametrization[v].subs({self._parameter: value})
-                )
-        return res
+                placement = placement.evalf()
+            realization[v] = placement
+        return realization
 
     def __str__(self) -> str:
         res = super().__str__() + " with motion defined for every vertex:"
         for vertex, param in self._parametrization.items():
             res = res + "\n" + str(vertex) + ": " + str(param)
         return res
-        return (
-            super().__str__() + " with motion defined for every vertex: " + str(self._parametrization)
-        )
+
+    def animate(self) -> None:
+        """
+        Animation of the parametric motion for parameter
+        in the range specified in the constructor.
+        """
+
+        raise NotImplementedError()
