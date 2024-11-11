bdd3e80d3800f2dfa8fcfc295254cde076ac9b9c
Matteo  Gallet <matteo.gallet@units.it>
2024-11-11 11:00:36 +0100

    Added example by Sean that now works correctly.

--- pyrigi/framework.py
+++ pyrigi/framework.py
@@ -1125,7 +1125,20 @@ class Framework(object):
         [0],
         [0],
         [0]])]
-        """
+        >>> F = Framework(Graph([[0, 1], [0, 3], [0, 4], [1, 3], [1, 4], [2, 3], [2, 4]]), {0: [0, 0], 1: [0, 1], 2: [0, 2], 3: [1, 2], 4: [-1, 2]})
+        >>> F.inf_flexes()
+        [Matrix([
+        [0],
+        [0],
+        [0],
+        [0],
+        [0],
+        [1],
+        [0],
+        [0],
+        [0],
+        [0]])]
+        """  # noqa: E501
         rigidity_matrix = self.rigidity_matrix(
             vertex_order=vertex_order, edge_order=edge_order
         )
