87eec8f87bde96fbd4625040816fc2a06fdf4a91
Matthias Himmelmann <65544132+matthiashimmelmann@users.noreply.github.com>
2024-11-15 08:34:26 +0100

    Matteo's comment

--- pyrigi/graph.py
+++ pyrigi/graph.py
@@ -1966,12 +1966,13 @@ class Graph(nx.Graph):
 
         V = self.number_of_nodes()
         E = self.number_of_edges()
+        # Only the complete graph is rigid in all dimensions
         if E == V * (V - 1) / 2:
             return math.inf
-        # Find the larger root of the Maxwell function E = d*V-d*(d+1)/2
-        dim = int(math.ceil(0.5 * (2 * V + math.sqrt((1 - 2 * V) ** 2 - 8 * E) - 1)))
+        # Find the largest d suchso that d*(d+1)/2 - d*V + E = 0
+        max_dim = int(math.ceil(0.5 * (2 * V + math.sqrt((1 - 2 * V) ** 2 - 8 * E) - 1)))
 
-        for d in range(dim, 0, -1):
+        for d in range(max_dim, 0, -1):
             if self.is_rigid(d, combinatorial=False):
                 return d
         return 0
