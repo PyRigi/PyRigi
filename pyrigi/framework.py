9a44de073c8e9b9278d1fe654e4e99dd00de13df
Jan Legersky <jan.legersky@gmail.com>
2024-12-12 15:14:48 +0100

    minor fix in a docstring

--- pyrigi/framework.py
+++ pyrigi/framework.py
@@ -2143,7 +2143,7 @@ class Framework(object):
         vertex_order:
             A list of vertices determining the internal vertex order.
         numerical:
-            A Boolean determining whether the evaluation of the product of the `stress`
+            A Boolean determining whether the evaluation of the product of the `inf_flex`
             and the rigidity matrix is symbolic or numerical.
         tolerance:
             Absolute tolerance that is the threshold for acceptable numerical flexes.
