2b2f96721de89dc442ed5935aa322ef233e70677
matthiashimmelmann <matthias.himmelmann@outlook.de>
2025-01-14 21:00:20 +0100

    PEP8 conformization

--- pyrigi/framework.py
+++ pyrigi/framework.py
@@ -2405,7 +2405,7 @@ class Framework(object):
             The SONC Criterion says that all faces on the boundary of the
             Newton polytope need to satisfy the SONC property. It simplifies to
             $|c_{ij}| <= sqrt(4*c_{ii}*c_{jj})$ for all $i,j$ for coefficients $c_{ij}$.
-            In addition, $c_{ii}$ and $c_{jj}$ need to have the same sign or be zero. 
+            In addition, $c_{ii}$ and $c_{jj}$ need to have the same sign or be zero.
             """
             return all(
                 [
@@ -2429,103 +2429,6 @@ class Framework(object):
             ) and not all(
                 [sp.sign(coefficients[(i, i)]) == 0 for i in range(len(inf_flexes))]
             )
-        """
-        # Otherwise we utilize the stress matrix criterion by Connelly.
-        a = sp.symbols("a0:%s" % len(stresses), real=True)
-        stress_matrices = [
-            sp.zeros(len(inf_flexes), len(inf_flexes)) for _ in range(len(stresses))
-        ]
-        # We start by parametrizing the space of all stress matrices with linear
-        # factors `a`. The positive definiteness criterion turns the linear forms
-        # into polynomials in `a`.
-        for i in range(len(stresses)):
-            stress = stresses[i].transpose().tolist()[0]
-            for j1 in range(len(inf_flexes)):
-                for j2 in range(len(inf_flexes)):
-                    flex1 = self._transform_inf_flex_to_pointwise(inf_flexes[j1])
-                    flex2 = self._transform_inf_flex_to_pointwise(inf_flexes[j2])
-                    stress_matrices[i][j1, j2] = a[i] * sum(
-                        [
-                            sum(
-                                v
-                                for v in (
-                                    stress[k]
-                                    * Matrix(
-                                        [
-                                            v - w
-                                            for v, w in zip(
-                                                flex1[edges[k][0]], flex1[edges[k][1]]
-                                            )
-                                        ]
-                                    ).transpose()
-                                    * Matrix(
-                                        [
-                                            v - w
-                                            for v, w in zip(
-                                                flex2[edges[k][0]], flex2[edges[k][1]]
-                                            )
-                                        ]
-                                    )
-                                )
-                            )
-                            for k in range(self._graph.number_of_edges())
-                        ]
-                    )
-        stress_matrix_sum = sum(
-            stress_matrices, sp.zeros(len(inf_flexes), len(inf_flexes))
-        )
-        # To check the positive definiteness of stress_matrix_sum, we use Sylvester's
-        # criterion on `stress_matrix_sum`, stating that all upper-left minors have a
-        # positive determinant. To certify nonnegativity in the resulting system
-        # of equations, we introduce variables `b` so that $eq >= 0$ if and only if
-        # $eq = {b_i}^2$ for $b_i in R$. We proceed to calculate the zeros
-        # of this homogeneous polynomial system of equations.
-        # b = sp.symbols("b0:%s" % stress_matrix_sum.shape[0], real=True)
-        polysys_sols = sp.solve(
-            [
-                sp.det(stress_matrix_sum[0 : i + 1, 0 : i + 1]) - b[i] ** 2
-                for i in range(stress_matrix_sum.shape[0])
-            ]
-        )
-        if not polysys_sols:
-            return False
-        polysys_sols_b_eval = []
-        for sol in polysys_sols:
-            # If this system has a real, nontrivial zero, then we have
-            # already found a positive definite stress matrix by construction
-            # through the insertion of the solution into `stress_matrix_sum`.
-            if all([sp.im(s).is_zero for s in sol.values()]) and any(
-                [not sp.re(s).is_zero for s in sol.values()]
-            ):
-                return True
-            # Else, we check whether we can let the imaginary part vanish through
-            # the selection of the values of `b`.
-            b_sols = sp.solve([sp.im(s) for s in sol.values()])
-            b_sols_no_imag = sum(
-                [
-                    sp.solve([sp.im(entry) for entry in b_sol.values()])
-                    for b_sol in b_sols
-                ],
-                [],
-            )
-            for entry in b_sols_no_imag:
-                polysys_sols_b_eval += [
-                    {key: value.subs(entry) for key, value in b_sol.items()} | entry
-                    for b_sol in b_sols
-                ]
-
-        def filter_zeros(entry):
-            return all([not sp.sympify(t).is_zero for t in entry.values()])
-
-        filtered_list = list(filter(filter_zeros, polysys_sols_b_eval))
-        # If all solutions obtained by letting the imaginary part vanish are trivial
-        # -- in a homogeneous system, the origin is always a solution -- then there
-        # exists no positive definite stress matrix for this system.
-        if not filtered_list:
-            return False
-        else:
-            return True
-        """
         raise AttributeError(
             "Prestress stability is not yet implemented for the general case."
         )
@@ -2574,67 +2477,13 @@ class Framework(object):
         """  # noqa: E501
         stresses = self.stresses()
         inf_flexes = self.inf_flexes()
-        edges = self._graph.edge_list()
         if self.is_inf_rigid():
             return True
         if len(inf_flexes) == 0 or len(stresses) == 0:
             return False
         if len(stresses) == 1 or len(inf_flexes) == 1:
             return self.is_prestress_stable()
-        """
-        # When there are more than one stress and flex, we use the polynomial system
-        # described by the stress energy to test the second-order rigidity of the
-        # framework.
-        a = sp.symbols("a0:%s" % len(inf_flexes), real=True)
-        b = sp.symbols("b0:%s" % len(stresses), real=True)
-        stress_energy = 0
-        for i in range(len(stresses)):
-            Q = 0
-            stress = stresses[i].transpose().tolist()[0]
-            for j in range(len(inf_flexes)):
-                flex = self._transform_inf_flex_to_pointwise(inf_flexes[j])
-                Q += sum(
-                    [
-                        sum(
-                            v
-                            for v in [
-                                stress[k] * (a[j] * (v - w)) ** 2
-                                for v, w in zip(flex[edges[k][0]], flex[edges[k][1]])
-                            ]
-                        )
-                        for k in range(self._graph.number_of_edges())
-                    ]
-                )
-            stress_energy = stress_energy + b[i] * Q.simplify()
-        stress_energy = sp.Poly(stress_energy.simplify(), b)
-        # From the `stress_energy`, we extract the coefficients of the variable `b[i]`
-        # for each `i`. If the resulting polynomial system has no solution in `sympy`,
-        # then the corresponding vanishing ideal is either equal to $<0>$ or $<1>$. We
-        # test against the latter case, as it would imply that there is no solution,
-        # while the former case implies that everything is a solution.
-        poly_sys_in_a = [stress_energy.coeff_monomial(b[i] ** 1) for i in range(len(b))]
-        if all([poly.is_zero for poly in poly_sys_in_a]):
-            return False
-        a_sols = sp.solve(poly_sys_in_a, a)
-        if not a_sols:
-            return True
-        real_a_sol_part = [[sp.re(s[t]) for t in range(len(s))] for s in a_sols]
-        imag_a_sol_part = [[sp.im(s[t]) for t in range(len(s))] for s in a_sols]
-        imag_a_sols = [sp.solve(sol) for sol in imag_a_sol_part]
-        for i in range(len(real_a_sol_part)):
-            local_imag_a_sols = (
-                [imag_a_sols[i]] if isinstance(imag_a_sols[i], dict) else imag_a_sols[i]
-            )
-            for j in range(len(local_imag_a_sols)):
-                if not all(
-                    [
-                        sp.sympify(s).subs(local_imag_a_sols[j]).is_zero
-                        for s in real_a_sol_part[i]
-                    ]
-                ):
-                    return False
-        return True
-        """
+
         raise AttributeError(
             "Second-order rigidity is not implemented for this framework."
         )
