625f6a9ce01ec11e65c92c806125c3e0a4b168d3
matthiashimmelmann <matthias.himmelmann@outlook.de>
2025-01-17 11:30:07 +0100

    Adressed Jan's comments

--- test/test_frameworkDB.py
+++ test/test_frameworkDB.py
@@ -1,5 +1,5 @@
 import pyrigi.frameworkDB as fws
-import numpy as np
+import sympy as sp
 from math import isclose
 
 import pytest
@@ -11,7 +11,7 @@ def test_Cycle():
 
     F = fws.Cycle(5, dim=1)
     assert (
-        F._dim == 1
+        F.dim() == 1
         and F._graph.number_of_nodes() == 5
         and F._graph.number_of_edges() == 5
         and F.is_inf_rigid()
@@ -19,36 +19,28 @@ def test_Cycle():
 
     F = fws.Cycle(4, dim=2)
     assert (
-        F._dim == 2
+        F.dim() == 2
         and F._graph.number_of_nodes() == 4
         and F._graph.number_of_edges() == 4
         and all(
-            [
-                np.linalg.norm(p) == 1
-                for p in F.realization(as_points=True, numerical=True).values()
-            ]
+            [(L - sp.sympify("sqrt(2)")).is_zero for L in F.edge_lengths().values()]
         )
         and len(F.inf_flexes()) == 1
     )
 
     F = fws.Cycle(6, dim=5)
     assert (
-        F._dim == 5
+        F.dim() == 5
         and F._graph.number_of_nodes() == 6
         and F._graph.number_of_edges() == 6
-        and all(
-            [
-                sum(p) == 0 or sum(p) == 1
-                for p in F.realization(as_points=True, numerical=True).values()
-            ]
-        )
+        and all([L in [1, sp.sympify("sqrt(2)")] for L in F.edge_lengths().values()])
     )
 
 
 def test_Square():
     F = fws.Square()
     assert (
-        F._dim == 2
+        F.dim() == 2
         and F._graph.number_of_nodes() == 4
         and F._graph.number_of_edges() == 4
         and len(F.inf_flexes()) == 1
@@ -58,9 +50,7 @@ def test_Square():
 def test_Diamond():
     F = fws.Diamond()
     assert (
-        F._dim == 2
-        and F.is_congruent_realization(fws.Square().realization(as_points=True))
-        and F.is_equivalent_realization(fws.Square().realization(as_points=True))
+        F.dim() == 2
         and F._graph.number_of_nodes() == 4
         and F._graph.number_of_edges() == 5
         and F.is_inf_rigid()
@@ -70,7 +60,7 @@ def test_Diamond():
 def test_Cube():
     F = fws.Cube()
     assert (
-        F._dim == 3
+        F.dim() == 3
         and F._graph.number_of_nodes() == 8
         and F._graph.number_of_edges() == 12
         and len(F.inf_flexes()) == 6
@@ -80,7 +70,7 @@ def test_Cube():
 def test_Octahedron():
     F = fws.Octahedron()
     assert (
-        F._dim == 3
+        F.dim() == 3
         and F._graph.number_of_nodes() == 6
         and F._graph.number_of_edges() == 12
         and len(F.inf_flexes()) == 0
@@ -88,7 +78,7 @@ def test_Octahedron():
 
     F = fws.Octahedron(realization="Bricard_line")
     assert (
-        F._dim == 3
+        F.dim() == 3
         and F._graph.number_of_nodes() == 6
         and F._graph.number_of_edges() == 12
         and len(F.inf_flexes()) == 1
@@ -97,7 +87,7 @@ def test_Octahedron():
 
     F = fws.Octahedron(realization="Bricard_plane")
     assert (
-        F._dim == 3
+        F.dim() == 3
         and F._graph.number_of_nodes() == 6
         and F._graph.number_of_edges() == 12
         and len(F.inf_flexes()) == 1
@@ -108,7 +98,7 @@ def test_Octahedron():
 def test_K33plusEdge():
     F = fws.K33plusEdge()
     assert (
-        F._dim == 2
+        F.dim() == 2
         and F._graph.number_of_nodes() == 6
         and F._graph.number_of_edges() == 10
         and F.is_inf_rigid()
@@ -121,7 +111,7 @@ def test_Complete():
 
     F = fws.Complete(5, dim=1)
     assert (
-        F._dim == 1
+        F.dim() == 1
         and F._graph.number_of_nodes() == 5
         and F._graph.number_of_edges() == 10
         and F.is_inf_rigid()
@@ -129,7 +119,7 @@ def test_Complete():
 
     F = fws.Complete(4, dim=2)
     assert (
-        F._dim == 2
+        F.dim() == 2
         and F._graph.number_of_nodes() == 4
         and F._graph.number_of_edges() == 6
         and F.is_inf_rigid()
@@ -138,7 +128,7 @@ def test_Complete():
 
     F = fws.Complete(3, dim=3)
     assert (
-        F._dim == 3
+        F.dim() == 3
         and F._graph.number_of_nodes() == 3
         and F._graph.number_of_edges() == 3
         and F.is_inf_rigid()
@@ -152,7 +142,7 @@ def test_Path():
 
     F = fws.Path(5, dim=1)
     assert (
-        F._dim == 1
+        F.dim() == 1
         and F._graph.number_of_nodes() == 5
         and F._graph.number_of_edges() == 4
         and F.is_inf_rigid()
@@ -160,7 +150,7 @@ def test_Path():
 
     F = fws.Path(4, dim=2)
     assert (
-        F._dim == 2
+        F.dim() == 2
         and F._graph.number_of_nodes() == 4
         and F._graph.number_of_edges() == 3
         and len(F.inf_flexes()) == 2
@@ -168,7 +158,7 @@ def test_Path():
 
     F = fws.Path(3, dim=3)
     assert (
-        F._dim == 3
+        F.dim() == 3
         and F._graph.number_of_nodes() == 3
         and F._graph.number_of_edges() == 2
         and len(F.inf_flexes()) == 1
@@ -178,7 +168,7 @@ def test_Path():
 def test_ThreePrism():
     F = fws.ThreePrism()
     assert (
-        F._dim == 2
+        F.dim() == 2
         and F._graph.number_of_edges() == 9
         and F._graph.number_of_nodes() == 6
         and F.is_inf_rigid()
@@ -186,7 +176,7 @@ def test_ThreePrism():
 
     F = fws.ThreePrism(realization="flexible")
     assert (
-        F._dim == 2
+        F.dim() == 2
         and F._graph.number_of_edges() == 9
         and F._graph.number_of_nodes() == 6
         and len(F.inf_flexes()) == 1
@@ -195,7 +185,7 @@ def test_ThreePrism():
 
     F = fws.ThreePrism(realization="parallel")
     assert (
-        F._dim == 2
+        F.dim() == 2
         and F._graph.number_of_edges() == 9
         and F._graph.number_of_nodes() == 6
         and len(F.inf_flexes()) == 1
@@ -206,7 +196,7 @@ def test_ThreePrism():
 def test_ThreePrismPlusEdge():
     F = fws.ThreePrismPlusEdge()
     assert (
-        F._dim == 2
+        F.dim() == 2
         and F._graph.number_of_edges() == 10
         and F._graph.number_of_nodes() == 6
         and F.is_inf_rigid()
@@ -218,24 +208,12 @@ def test_CompleteBipartite():
         fws.CompleteBipartite(1.5, 2.5)
 
     F = fws.CompleteBipartite(3, 4)
-    print(
-        [
-            [np.linalg.norm(p), np.linalg.norm([p[0] - 1, p[1] - 3])]
-            for p in F.realization(as_points=True, numerical=True).values()
-        ]
-    )
+    print(F.edge_lengths().values())
     assert (
-        F._dim == 2
+        F.dim() == 2
         and F._graph.number_of_nodes() == 7
         and F._graph.number_of_edges() == 12
         and F.is_inf_rigid()
-        and all(
-            [
-                isclose(np.linalg.norm(p), 1, rel_tol=1e-10)
-                or isclose(np.linalg.norm([p[0] - 1, p[1] - 3]), 2, rel_tol=1e-10)
-                for p in F.realization(as_points=True, numerical=True).values()
-            ]
-        )
     )
 
 
@@ -245,7 +223,7 @@ def test_Frustum():
 
     F = fws.Frustum(3)
     assert (
-        F._dim == 2
+        F.dim() == 2
         and F._graph.number_of_nodes() == 6
         and F._graph.number_of_edges() == 9
         and len(F.inf_flexes()) == 1
@@ -254,7 +232,7 @@ def test_Frustum():
 
     F = fws.Frustum(4)
     assert (
-        F._dim == 2
+        F.dim() == 2
         and F._graph.number_of_nodes() == 8
         and F._graph.number_of_edges() == 12
         and len(F.inf_flexes()) == 2
@@ -269,13 +247,14 @@ def test_CnSymmetricFourRegular():
 
     F = fws.CnSymmetricFourRegular(8)
     assert (
-        F._dim == 2
+        F.dim() == 2
         and F._graph.number_of_nodes() == 8
         and F._graph.number_of_edges() == 16
         and all(
             [
-                np.linalg.norm(p) == 1
-                for p in F.realization(as_points=True, numerical=True).values()
+                (L - sp.sympify("sqrt((1 - sqrt(2)/2)**2 + 1/2)")).is_zero
+                or (L - sp.sympify("sqrt((1 + sqrt(2)/2)**2 + 1/2)")).is_zero
+                for L in F.edge_lengths().values()
             ]
         )
     ) and all([F._graph.degree[v] == 4 for v in F._graph.nodes])
@@ -288,14 +267,26 @@ def test_CnSymmetricFourRegularWithFixedVertex():
 
     F = fws.CnSymmetricFourRegularWithFixedVertex(8)
     assert (
-        F._dim == 2
+        F.dim() == 2
         and F._graph.number_of_nodes() == 17
         and F._graph.number_of_edges() == 40
         and all(
             [
-                np.linalg.norm(p) in [0, 1]
-                or isclose(np.linalg.norm(p), 2.0591260281974, rel_tol=1e-8)
-                for p in F.realization(as_points=True, numerical=True).values()
+                any(
+                    [
+                        (L - z).is_zero
+                        for z in sp.sympify(
+                            [
+                                "sqrt((1 - sqrt(2)/2)**2 + 1/2)",
+                                "sqrt(106)/5",
+                                "2*sqrt(53)/5",
+                                "sqrt(1/2 + (sqrt(2)/2 + 1)**2)",
+                                "sqrt(41)/5",
+                            ]
+                        )
+                    ]
+                )
+                for L in F.edge_lengths().values()
             ]
         )
     ) and all([F._graph.degree[v] in [4, 5, 8] for v in F._graph.nodes])
