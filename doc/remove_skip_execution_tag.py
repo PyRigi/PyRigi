4cf6eca09ef98da58e582859fba874972c331175
Jan Legersky <jan.legersky@gmail.com>
2025-01-28 14:49:31 +0100

    locally skipped cells appear in the online doc

---
+++ doc/remove_skip_execution_tag.py
@@ -0,0 +1,15 @@
+# this script removes the tag 'skip-execution'
+# from all tutorial notebooks so these cells
+# appear in the online documentation
+
+import glob
+import os
+path = os.path.join(os.path.dirname(__file__), 'userguide/tutorials/')
+
+for filename in glob.iglob(path + '**/*.md', recursive=True):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    with open(filename, 'w') as file:
+        for line in lines:
+            if 'skip-execution' not in line:
+                file.write(line)
