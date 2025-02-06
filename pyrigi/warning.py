7de01e1226167582eafeeb9ff2d8185b80e2fd51
Jan Legersky <jan.legersky@gmail.com>
2025-02-06 10:34:06 +0100

    PEP8

--- pyrigi/warning.py
+++ pyrigi/warning.py
@@ -19,12 +19,19 @@ class RandomizedAlgorithmWarning(UserWarning):
         if msg is not None:
             super().__init__(msg, *args)
         else:
-            msg_str = f"The method {method.__qualname__} uses a randomized algorithm, see its docstring for more information!"
+            msg_str = (
+                f"The method {method.__qualname__} uses a randomized algorithm,"
+                + "see its docstring for more information!"
+            )
             if explicit_call is not None:
-                msg_str += f"\nIf the parameter `{explicit_call}` is used explicitly, this warning is not displayed."
+                msg_str += (
+                    f"\nIf the parameter `{explicit_call}` is used explicitly,"
+                    + "this warning is not displayed."
+                )
             if class_off is not None:
                 msg_str += (
-                    f"\nTo switch off all RandomizedAlgorithmWarnings for the class {class_off.__name__},"
+                    "\nTo switch off all RandomizedAlgorithmWarnings"
+                    + f"for the class {class_off.__name__},"
                     + f" use `{class_off.__name__}.silence_rand_alg_warns=True`."
                 )
             msg_str += "\n"
