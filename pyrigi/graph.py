fb922f57a97cac1f63a707eabde48bea884b9a1f
Mafalda Dal Cin <dalcin.mafalda@gmail.com>
2025-02-09 11:03:05 +0100

    correction block_3_2 method

--- pyrigi/graph.py
+++ pyrigi/graph.py
@@ -3123,19 +3123,22 @@ class Graph(nx.Graph):
         >>> G.block_3_2(0,11)
         Graph with vertices [0, 1, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14] and edges [[0, 1], [0, 5], [0, 7], [1, 4], [1, 7], [4, 5], [4, 8], [4, 11], [5, 6], [5, 8], [5, 14], [6, 10], [6, 11], [6, 12], [7, 8], [7, 13], [8, 12], [10, 13], [10, 14], [11, 12], [13, 14]]
         """  # noqa: E501
-        cut = next(nx.all_node_cuts(self))
-        if len(cut) >= 3:
+        try:
+            cut = next(nx.all_node_cuts(self))
+            if len(cut) >= 3:
+                return self
+            H = self.copy()
+            H.delete_vertices(cut)
+            calc_comp = nx.connected_components(H)
+            for conn_comp in calc_comp:
+                conn_comp.update(cut)
+                if u in conn_comp and v in conn_comp:
+                    break
+            B = nx.subgraph(self, conn_comp).copy()
+            B.add_edge(*(cut))
+            return B.block_3_2(u, v)
+        except StopIteration:
             return self
-        H = self.copy()
-        H.delete_vertices(cut)
-        calc_comp = nx.connected_components(H)
-        for conn_comp in calc_comp:
-            conn_comp.update(cut)
-            if u in conn_comp and v in conn_comp:
-                break
-        B = nx.subgraph(self, conn_comp).copy()
-        B.add_edge(*(cut))
-        return B.block_3_2(u, v)
 
     @doc_category("Rigidity Matroid")
     def R2_fundamental_circuit(self, u: Vertex, v: Vertex):
