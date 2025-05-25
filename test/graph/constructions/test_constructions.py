11e7594f7b1fa6307c3a6e49aebb61f39d9f5afc
Matteo Gallet <matteo.gallet@units.it>
2025-05-25 11:42:54 +0200

    Split constructions from graph.py.

---
+++ test/graph/constructions/test_constructions.py
@@ -0,0 +1,19 @@
+import pyrigi.graphDB as graphs
+from pyrigi.graph._constructions import constructions as graph_constructions
+from test import TEST_WRAPPED_FUNCTIONS
+
+
+def test_cone():
+    G = graphs.Complete(5).cone()
+    assert set(G.nodes) == set([0, 1, 2, 3, 4, 5]) and len(G.nodes) == 6
+    G = graphs.Complete(4).cone(vertex="a")
+    assert "a" in G.nodes
+    G = graphs.Cycle(4).cone()
+    assert G.number_of_nodes() == G.max_degree() + 1
+    if TEST_WRAPPED_FUNCTIONS:
+        G = graph_constructions.cone(graphs.Complete(5))
+        assert set(G.nodes) == set([0, 1, 2, 3, 4, 5]) and len(G.nodes) == 6
+        G = graph_constructions.cone(graphs.Complete(4), vertex="a")
+        assert "a" in G.nodes
+        G = graph_constructions.cone(graphs.Cycle(4))
+        assert G.number_of_nodes() == G.max_degree() + 1
