cacc06a550c263fc288c1ea1ae0a9254b317c234
ggmath <32544257+ggmath@users.noreply.github.com>
2026-04-23 20:01:05 +0200

    Feature: Extended realization counting (#378)
    * Added counting for non-minimally rigid graphs
    
    * Added counting for dim=1
    
    * Moved counting to separate file
    
    * Added spherical counting
    
    * Moved and updated tests
    
    * Fixed problem on small graphs
    
    * Added more documentation
    
    * Added plane counting in pyrigi
    
    * Added more tests and fixed issues
    
    * Separated rigid count
    
    * Added more tests
    
    * Added reference
    
    * Replaced deepcopy by copy for speedup
    
    * Deleted old version of realization counting
    
    * Added more_itertools among the dependencies.
    
    * Membership test via 'not in'.
    
    * Simplified code.
    
    * Sorted imports.
    
    * Avoided to overwrite Python sum function.
    
    * Simplified imports.
    
    * Flattened nested 'max' calls.
    
    * Improved single element slice without using list.
    
    * Less use of concatenation.
    
    * Removed unnecessary commands after 'return'.
    
    * Removed unnecessary commands after 'return'.
    
    * Small changes in number_of_realizations docstring.
    
    * Substituted len(G.vertex_list()) by G.number_of_nodes().
    
    * Removed unnecessary parameter.
    
    * More precise docstring including dimension.
    
    * Fixed typography.
    
    * Small improvements in docstrings.
    
    * Removed + in long error strings.
    
    * More Pythonic programming style.
    
    * Use number_connected_components from networkx.
    
    * Corrected markers of tests using lnumber
    
    * Added missing dimension value
    
    * Added testing without lnumber on merge to dev
    
    * Fixed testing without lnumber
    
    * Fixed testing without lnumber
    
    * Changed to return sympy infinity
    
    * Added testing without lnumber on merge to main
    
    * Fixed signature
    
    * Added check for possible values of algorithm
    
    * Added test for globally rigid graphs
    
    * Specified doc for lnumber algorithm
    
    * Renamed algorithm for internal computation
    
    * Added functionality for higher dimension in specific cases
    
    * Added test for single vertex graph
    
    * Added test on pseudo laman property
    
    * Added details in the docs
    
    * Fixed typos
    
    ---------
    
    Co-authored-by: Matteo Gallet <matteo.gallet@units.it>
    Co-authored-by: Jan Legersky <jan.legersky@gmail.com>

--- test/__init__.py
+++ test/__init__.py
@@ -1,1 +1,9 @@
 TEST_WRAPPED_FUNCTIONS = True
+
+
+def is_marker_selected(config, marker_name: str) -> bool:
+    """Return if the given marker is selected."""
+    expr = config.getoption("-m")
+    if not expr:  # no filtering so everything selected
+        return True
+    return marker_name in expr and f"not {marker_name}" not in expr
