16416164d8db54c90cc5d9765df9183b15f98106
Jan Legersky <jan.legersky@gmail.com>
2026-06-22 10:57:13 +0200

    Added tests showing a bug in global rigidity decoding

--- test/graphDB/test_cross_check.py
+++ test/graphDB/test_cross_check.py
@@ -1,11 +1,17 @@
 """Tests cross-checking databases of rigid graphs."""
 
 import networkx as nx
+from pathlib import Path
 import pytest
 
-from pyrigi.graph._rigidity.generic import is_rigid
+from pyrigi.graph._rigidity.generic import is_min_rigid, is_rigid
+from pyrigi.graph._rigidity.global_ import is_globally_rigid
 from pyrigi.graphDB import GraphStoreService, QueryFilter
 from pyrigi.graphDB.models import not_
+
+
+RIGIDITY_TYPES = ["rigidity", "min_rigidity", "global_rigidity"]
+RIGIDITY_FUNCTIONS = [is_rigid, is_min_rigid, is_globally_rigid]
 
 CONNECTED_NUMS = [0, 1, 1, 2, 6, 21, 112, 853, 11117, 261080, 11716571]
 # CONNECTED_NUMS[i] is the number of connected graphs with i vertices
@@ -30,20 +36,33 @@ DIM_RIGID_NUMS = {
 # DIM_RIGID_NUMS[dim][n] is the number of dim-graphs with n vertices
 
 
-@pytest.fixture(params=[6], scope="session")
+@pytest.fixture(
+    params=[
+        6,
+        pytest.param(7, marks=pytest.mark.long_local),
+    ],
+    scope="session",
+)
 def store_connected(request):
     """In-memory store of connected graphs."""
     store = GraphStoreService(":memory:").init()
     for n in range(2, request.param + 1):
-        store.ingest(f"test/graphDB/connected_graphs/graphs_{n}.g6")
-    store.populate_column("rigidity")
+        store.ingest(str(Path(__file__).parent / "connected_graphs" / f"graphs_{n}.g6"))
+
+    for rigidity_type in RIGIDITY_TYPES:
+        store.populate_column(rigidity_type)
+
     yield store
     store.close()
 
 
-def test_populate_rigidity(store_connected):
+@pytest.mark.parametrize(
+    "rigidity_type",
+    RIGIDITY_TYPES,
+)
+def test_populating(store_connected, rigidity_type):
     rows = store_connected.fetch(
-        filters=[QueryFilter("rigidity", "IS NULL")],
+        filters=[QueryFilter(rigidity_type, "IS NULL")],
     )
     assert len(rows) == 0
 
@@ -74,42 +93,59 @@ def test_rigid_nums_fetched(store_connected):
             assert len(rows) == DIM_RIGID_NUMS[dim][n]
 
 
-def test_rigid_nums_computed(store_connected):
-    num_vert = max(
-        [row["num_vertices"] for row in store_connected.fetch(select=["num_vertices"])]
-    )
-    for dim in range(1, num_vert + 1):
-        for n in range(2, num_vert + 1):
-            rows = store_connected.iter_fetch(
-                filters=[QueryFilter("num_vertices", "=", n)], select=["graph"]
+# def test_rigid_nums_computed(store_connected):
+#     """This is only to verify DIM_RIGID_NUMS."""
+#     num_vert = max(
+#         [row["num_vertices"] for row in store_connected.fetch(select=["num_vertices"])]
+#     )
+#     for dim in range(1, num_vert + 1):
+#         for n in range(2, num_vert + 1):
+#             rows = store_connected.iter_fetch(
+#                 filters=[QueryFilter("num_vertices", "=", n)], select=["graph"]
+#             )
+#             cnt = 0
+#             for row in rows:
+#                 G = nx.from_graph6_bytes(row["graph"].encode())
+#                 if is_rigid(G, dim=dim):
+#                     cnt += 1
+#             assert cnt == DIM_RIGID_NUMS[dim][n]
+
+
+@pytest.mark.parametrize(
+    "rigidity_type, rigidity_function",
+    zip(RIGIDITY_TYPES, RIGIDITY_FUNCTIONS),
+)
+def test_fetched_rigid_are_rigid(store_connected, rigidity_type, rigidity_function):
+    for dim in range(1, 6):
+        rows = store_connected.iter_fetch(
+            filters=[QueryFilter(rigidity_type, "=", dim)],
+            select=["graph", rigidity_type],
+        )
+        for row in rows:
+            G = nx.from_graph6_bytes(row["graph"].encode())
+            assert rigidity_function(G, dim=dim), (
+                f"Graph with g6 encoding '{row["graph"]}' was fetched "
+                f"as {dim}-{rigidity_type.replace("dity", "d")}, but it is not. "
+                f"The stored '{rigidity_type}' value is {row[rigidity_type]}."
             )
-            cnt = 0
-            for row in rows:
-                G = nx.from_graph6_bytes(row["graph"].encode())
-                if is_rigid(G, dim=dim):
-                    cnt += 1
-            assert cnt == DIM_RIGID_NUMS[dim][n]
 
 
-def test_fetched_rigid_are_rigid(store_connected):
+@pytest.mark.parametrize(
+    "rigidity_type, rigidity_function",
+    zip(RIGIDITY_TYPES, RIGIDITY_FUNCTIONS),
+)
+def test_fetched_non_rigid_are_not_rigid(
+    store_connected, rigidity_type, rigidity_function
+):
     for dim in range(1, 6):
         rows = store_connected.iter_fetch(
-            filters=[QueryFilter("rigidity", "=", dim)], select=["graph"]
+            expr=not_(QueryFilter(rigidity_type, "=", dim)),
+            select=["graph", rigidity_type],
         )
         for row in rows:
             G = nx.from_graph6_bytes(row["graph"].encode())
-            assert is_rigid(
-                G, dim=dim
-            ), f"Graph {G} was fetched as {dim}-rigid, but it is {dim}-rigid."
-
-
-def test_fetched_flexible_are_flexible(store_connected):
-    for dim in range(1, 6):
-        rows = store_connected.iter_fetch(
-            expr=not_(QueryFilter("rigidity", "=", dim)), select=["graph"]
-        )
-        for row in rows:
-            G = nx.from_graph6_bytes(row["graph"].encode())
-            assert not is_rigid(
-                G, dim=dim
-            ), f"Graph {G} was fetched as {dim}-flexible, but it is {dim}-rigid."
+            assert not rigidity_function(G, dim=dim), (
+                f"Graph with g6 encoding '{row["graph"]}' was fetched as "
+                f"not {dim}-{rigidity_type.replace("dity", "d")}, but it is. "
+                f"The stored '{rigidity_type}' value is {row[rigidity_type]}."
+            )
